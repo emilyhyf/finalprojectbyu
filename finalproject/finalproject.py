08d59c370ef9a0bb758fd2c67e67fcd810dadff9
emilyhyf <68252398+emilyhyf@users.noreply.github.com>
2020-07-16 21:21:23 -0700

    Update finalproject.py
    Hi, please check if my first draft works. :)

---
+++ finalproject/finalproject.py
@@ -0,0 +1,100 @@
+import random
+
+
+def drawBoard(board):
+    # This function prints out the board that it was passed.
+
+    # "board" is a list of 10 strings representing the board (ignore index 0)
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+
+def playAgain():
+    # This function returns True if the player wants to play again, otherwise it returns False.
+    print('Do you want to play again? (yes or no)')
+    return input().lower().startswith('y')
+
+
+def makeMove(board, letter, move):
+    board[move] = letter
+
+
+def isWinner(bo, le):
+    # Given a board and a player's letter, this function returns True if that player has won.
+    # We use bo instead of board and le instead of letter so we don't have to type as much.
+    return ((bo[7] == le and bo[8] == le and bo[9] == le) or  # across the top
+            (bo[4] == le and bo[5] == le and bo[6] == le) or  # across the middle
+            (bo[1] == le and bo[2] == le and bo[3] == le) or  # across the bottom
+            (bo[7] == le and bo[4] == le and bo[1] == le) or  # down the left side
+            (bo[8] == le and bo[5] == le and bo[2] == le) or  # down the middle
+            (bo[9] == le and bo[6] == le and bo[3] == le) or  # down the right side
+            (bo[7] == le and bo[5] == le and bo[3] == le) or  # diagonal
+            (bo[9] == le and bo[5] == le and bo[1] == le))  # diagonal
+
+
+def isSpaceFree(board, move):
+    # Return true if the passed move is free on the passed board.
+    return board[move] == ' '
+
+
+def getPlayerMove(board):
+    # Let the player type in his move.
+    move = ' '
+    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+        print('What is your move? (1-9)')
+        move = input()
+    return int(move)
+
+
+def isBoardFull(board):
+    # Return True if every space on the board has been taken. Otherwise return False.
+    for i in range(1, 10):
+        if isSpaceFree(board, i):
+            return False
+    return True
+
+
+print('Welcome to Tic Tac Toe!')
+
+while True:
+    # Reset the board
+    theBoard = [' '] * 10
+    gameIsPlaying = True
+    currentPlayer = 'Player 1'
+    currentPlayerLetter = 'X'
+
+    while gameIsPlaying:
+        # Player's turn.
+        drawBoard(theBoard)
+        print(currentPlayer + ', it\'s your turn.')
+        move = getPlayerMove(theBoard)
+        makeMove(theBoard, currentPlayerLetter, move)
+
+        if isWinner(theBoard, currentPlayerLetter):
+            drawBoard(theBoard)
+            print('Hooray! ' + currentPlayer + ' has won the game!')
+            gameIsPlaying = False
+        else:
+            if isBoardFull(theBoard):
+                drawBoard(theBoard)
+                print('The game is a tie!')
+                break
+            else:
+                if (currentPlayer == 'Player 1'):
+                    currentPlayer = 'Player 2'
+                    currentPlayerLetter = 'O'
+                else:
+                    currentPlayer = 'Player 1'
+                    currentPlayerLetter = 'X'
+
+    if not playAgain():
+        break
